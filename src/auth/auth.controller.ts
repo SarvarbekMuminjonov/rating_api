8b88c32148dc4e31680f0102384bf5c13ae60e74
SarvarbekMuminjonov <smomimjonov20@gmail.com>
2022-09-30 12:31:07 +0500

    2 controllers added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,9 +4,9 @@ import { AuthDto } from './dto/auth.dto';
 @Controller('auth')
 export class AuthController {
   @Post('register')
-  async register(@Body autDto: AuthDto) {}
+  async register(@Body() dto: AuthDto) {}
 
   @HttpCode(200)
   @Post('login')
-  async login(@Body autDto: AuthDto) {}
+  async login(@Body() dto: AuthDto) {}
 }
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  HttpCode,
+} from '@nestjs/common';
+import { ProductModel } from './product.model';
+import { FindProductDto } from './dto/find-product.dto';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  @Post('create')
+  async create(@Body() dto: Omit<ProductModel, '_id'>) {}
+  @Get(':id')
+  async get(@Param() id: string) {}
+  @Delete(':id')
+  async delete(@Param() id: string) {}
+  @Patch(':id')
+  async patch(@Param() id: string, @Body() dto: ProductModel) {}
+  @HttpCode(200)
+  @Post()
+  async find(@Body() dto: FindProductDto) {}
+}
